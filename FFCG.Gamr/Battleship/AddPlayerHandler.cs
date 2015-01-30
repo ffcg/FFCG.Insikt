db21e711f34e5fbe95f64b43b8974d2786271eb3
Nina Stolze <nina.stolze@ffcg.se>
2015-01-30 19:21:14 +0100

    Added empty handler

---
+++ FFCG.Gamr/Battleship/AddPlayerHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class AddedPlayerViewModel
+    {
+        public Guid PlayerId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class AddPlayer : IRequest<AddedPlayerViewModel>
+    {
+        public string Name { get; set; }
+    }
+
+    public class AddPlayerHandler: IHandleRequests<AddPlayer, AddedPlayerViewModel>
+    {
+        public AddedPlayerViewModel Request(AddPlayer request)
+        {
+            //add player to game
+            //return added player info
+            //if other player exists, use signal r to notify both that game is ready to start
+            //if no other player exists, notify player that he needs to wait
+            return null;
+        }
+    }
+}
