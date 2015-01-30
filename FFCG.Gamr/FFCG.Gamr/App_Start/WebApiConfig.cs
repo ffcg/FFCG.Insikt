33db9428437c1bcc66d1b5a56fb02b8d194b034c
Nils Hjalmarson <nils.hjalmarson@gmail.com>
2015-01-30 19:11:47 +0100

    web API for battleship

---
+++ FFCG.Gamr/FFCG.Gamr/App_Start/WebApiConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace FFCG.Gamr
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute("DefaultApi", "battleship/{controller}/{id}", new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FFCG.Gamr/FFCG.Gamr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FFCG.Gamr.WebApiApplication" Language="C#" %>
