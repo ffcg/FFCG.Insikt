39cdc2856d477127bd92d33674911177749889de
Nils Hjalmarson <nils.hjalmarson@gmail.com>
2015-01-30 17:33:08 +0100

    Folders for battleship 2

---
+++ FFCG.Gamr/FFCG.Gamr/Battleship/BattleshipHub.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace FFCG.Gamr.Battleship
+{
+    public class BattleshipHub : Hub
+    {
+        public void Handshake()
+        {
+            
+        }
+
+        public override Task OnConnected()
+        {
+            var test = Clients.Caller;
+            return base.OnConnected();
+        }
+
+    }
+}
