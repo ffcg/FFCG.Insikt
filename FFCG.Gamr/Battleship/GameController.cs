e1c26a5714c7a7689044d060e551f7e580d0a6dd
Nina Stolze <nina.stolze@ffcg.se>
2015-01-30 22:42:56 +0100

    Added game controller for tracking current active game

---
+++ FFCG.Gamr/Battleship/GameController.cs
@@ -0,0 +1,23 @@
+﻿using Battleship.Domain;
+
+namespace Battleship
+{
+    public class GameController
+    {
+        private static readonly GameController Singleton = new GameController();
+        private Game _game;
+
+        private GameController()
+        {}
+
+        public static GameController Get()
+        {
+            return Singleton;
+        }
+
+        public Game GetCurrentGame()
+        {
+            return _game ?? (_game = new Game(5));
+        }
+    }
+}
