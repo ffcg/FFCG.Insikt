96120d2ced7f57d819013dffbc243e66f0cbeb1b
EgoDrama <it_david@hotmail.com>
2015-01-31 00:54:42 +0100

    added console app

---
+++ FFCG.Gamr/FFCG.Brun7.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFCG.Brun7.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var brun7Hub = new HubConnection("http://localhost:50843/");
+            var proxy = brun7Hub.CreateHubProxy("brun7Hub");
+
+            proxy.Invoke("StartGame", null).Wait();
+
+            //proxy.On<string, string>("Hello",(name, message) => System.Console.Write("Message: " + name + " message: " + message));
+            //brun7Hub.Start().Wait();
+        }
+    }
+    
+}
